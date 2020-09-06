654fe859a5943a5aa39e3cc8ab632e4e26fcc562
Diogo Freitas <diogofreitas1994@gmail.com>
2020-09-06 19:10:22 +0100

    Updates

---
+++ precache-manifest.02ad086d43ad6fe9443e1e05699ccd9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36513dc09f87dd78e90de706ee9236b0",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "bca318241c599c4ecd46",
+    "url": "/smartbrain/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "176bde11167d3b6493ca",
+    "url": "/smartbrain/static/js/2.662a77a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smartbrain/static/js/2.662a77a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bca318241c599c4ecd46",
+    "url": "/smartbrain/static/js/main.faa843a6.chunk.js"
+  },
+  {
+    "revision": "ae456e4515232efad5b0",
+    "url": "/smartbrain/static/js/runtime-main.b715ec47.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/smartbrain/static/media/logo.5d5d9eef.svg"
+  }
+]);
